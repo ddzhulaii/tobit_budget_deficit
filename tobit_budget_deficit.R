530cb06ceb5c2d00395fe8c6fd64fa8dc4c805e8
ddzhulaii <daryna.dzhulay@gmail.com>
2018-12-14 02:46:50 +0200

    initial commit

---
+++ tobit_budget_deficit.R
@@ -0,0 +1,32 @@
+library(data.table)
+library(VGAM)
+
+###Load data
+data = data.table(read.csv("./data/budget_deficit_data.csv"))
+#Set lower bound for Tobit model
+LOWER_BOUND <- 0
+
+#Count columns
+ncol(data)
+
+#Count records
+nrow(data)
+
+head(data)
+
+deficit_data = subset(data, select=c("DEFICIT_M", "BoP", "GOV_DEBT", "IMF_FINANCING", "TAX_REVENUE"))
+#Scaling data
+deficit_data$BoP <- deficit_data$BoP*(-1)
+deficit_data$TAX_REVENUE <- deficit_data$TAX_REVENUE/1000
+
+model = vglm(DEFICIT_M ~ BoP + GOV_DEBT + IMF_FINANCING + TAX_REVENUE, tobit(Lower = LOWER_BOUND), data = deficit_data)
+summary(model)
+
+#Count marginal effect
+xValues = c(1, colMeans(deficit_data)[2:ncol(deficit_data)])
+allPar = coef(model, logSigma = FALSE)
+beta = allPar[!names(allPar) %in% c("sigma")]
+sigma = allPar["sigma"]
+xBeta = crossprod(xValues, beta)
+zLeft = (-LOWER_BOUND + xBeta)/sigma
+beta[!names(beta) %in% c("(Intercept)")] * (pnorm(zLeft))
